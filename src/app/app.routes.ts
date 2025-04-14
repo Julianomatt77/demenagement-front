d410bf949cb3b7442526dec3c4c65b74e6dc1985
Julien MARTIN <ju.martin@outlook.com>
2025-04-14 15:29:15 +0200

    add cgu & error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import {HomepageComponent} from './components/page/homepage/homepage.component';
 import {LoginComponent} from './components/page/login/login.component';
+import {CguComponent} from './components/page/cgu/cgu.component';
+import {ErrorComponent} from './components/page/error/error.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   {path: 'login', component: LoginComponent},
   {path: 'register', component: LoginComponent, data:{registration:true}},
-  // { path: 'not-found', component: ErrorComponent },
-  // {path: 'cgu', component: CguComponent},
+  { path: 'not-found', component: ErrorComponent },
+  {path: 'cgu', component: CguComponent},
   // {path: 'contact', component: ContactComponent},
   { path: '**', redirectTo: 'not-found'}
 ];
